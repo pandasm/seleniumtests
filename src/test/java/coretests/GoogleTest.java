de6d6114c79c6268cf264ff7ee9fb82d4e25bdba
Soumya Mukherjee <mukherjee.soumya@gmail.com>
2019-10-31 22:55:35 +0530

    updated the code

--- src/test/java/coretests/GoogleTest.java
+++ src/test/java/coretests/GoogleTest.java
@@ -2,9 +2,15 @@ package coretests;
 
 import static org.testng.Assert.assertTrue;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,11 +18,13 @@ import org.testng.annotations.Test;
 public class GoogleTest {
 	
 	private WebDriver driver;
+	private WebDriverWait wait;
 	
 	@BeforeTest
 	public void setup(){	
 		System.setProperty("webdriver.chrome.driver", "/Users/soumyamukherjee/Downloads/chromedriver");
 		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 10);
 	}
 	
 	@Test
@@ -26,15 +34,23 @@ public class GoogleTest {
 		
 		//Enter Automation and then verify the link if automation is present
 		driver.findElement(By.name("q")).sendKeys("automation");
-		Thread.sleep(5000);
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK")));
 		driver.findElement(By.name("btnK")).click();
 		
-		Thread.sleep(5000);
-		String searchtext = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div[1]/a/h3")).getText();
-		if(searchtext.toLowerCase().contains("automation"))
-			assertTrue(true,"Automation word is present");
-		else
-			assertTrue(false,"Automation word is not present");
+		
+		List<WebElement> ele = driver.findElements(By.className("rc"));
+		System.out.println("Element Size : " + ele.size());
+		
+		for (WebElement el : ele){
+			String searchtext = el.getText().toLowerCase();
+			if(searchtext.contains("automation")){
+				assertTrue(true,"Automation word is present");
+				break;
+			}
+			else
+				assertTrue(false,"Automation word is not present");
+		}
 		
 	}
 	
